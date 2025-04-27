13b01fae46c62981cfb4b651575b264cb6cdaf2b
KUNDANYADAV7 <kdyd999@gmail.com>
2025-04-27 19:49:15 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,8 @@ app.use(
 );
 
 app.use(compression()); // ✅ Enables gzip compression for faster API & frontend responses
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
 // ✅ Serve static files (like images) from /public folder with 1 year cache
